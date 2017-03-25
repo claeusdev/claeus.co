029de22ad82b4a2f27a0cd30e1159ed7527cca6b
Nana Adjei Manu <claeusdev@outlook.com>
2017-03-25 00:34:07 +0000

    STyled homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,22 @@
 class PagesController < ApplicationController
+
+  def index
+
+  end
+
+  def services
+
+  end
+
+  def portfolio
+
+  end
+
+  def hire
+
+  end
+
+  def contact
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
 
-  root "posts#index"
+  get "portfolio", to: "pages#portfolio"
+  get "services", to: "pages#services"
+
+  get "hire-me", to: "pages#hire"
+  get "get-in-touch", to: "pages#contact"
+  root "pages#index"
 end
